c84039fcef7a68b8ced6dfefcfafc586f7059fdd
denosaur98 <denischurchill1998@gmail.com>
2024-12-06 13:51:31 +0300

    Revert "настроил билд"
    This reverts commit 0d50f7b359cb94b58cab5ebe0986e56a072c80dc.

--- templates/project/vite.config.js
+++ templates/project/vite.config.js
@@ -1,5 +1,7 @@
 import { defineConfig } from 'vite'
 import { resolve } from 'path'
+import { createHtmlPlugin } from 'vite-plugin-html'
+import stringPlugin from 'vite-plugin-string'
 
 export default defineConfig({
 	root: './src',
@@ -7,13 +9,23 @@ export default defineConfig({
 		outDir: '../output',
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, 'src/index.html'),
+				main: resolve(__dirname, 'src/app.html'),
 			},
 		},
 	},
 	server: {
 		port: 8080,
 	},
+	plugins: [
+		createHtmlPlugin({
+			minify: true,
+			entry: 'app.js',
+			template: 'app.html',
+		}),
+		stringPlugin({
+			include: '**/*.html',
+		}),
+	],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, './src'),
